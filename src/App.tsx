e791f3dd0322954177d1bd7f7f9e980647c27e04
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-14 19:07:16 -0300

    Adiciona StatusBar.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React, { FunctionComponent } from 'react'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 
-const App: FunctionComponent = () => <View />
+const App: FunctionComponent = () => (
+  <>
+    <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+  </>
+)
 
 export default App
