53e72fd71f528a9ba8951792a9436ac46ccb4101
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-14 21:10:18 -0300

    Cria estrutura básica para as páginas SignIn e SignUp.

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+
+import { Container } from './styles'
+
+const SignIn: FunctionComponent = () => {
+  return <Container />
+}
+export default SignIn
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,8 @@
+import React, { FunctionComponent } from 'react'
+
+import { Container } from './styles'
+
+const SignUp: FunctionComponent = () => {
+  return <Container />
+}
+export default SignUp
