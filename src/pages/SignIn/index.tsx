cbf2f992369d26ecdc6df194e17abe1efef3bae5
Leonardo Oliva Kraciunas <leonardo@okraciunas.com>
2020-06-17 18:43:51 -0300

    Adiciona botões de 'Esqueci minha senha' e 'Criar conta'.

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,23 +1,61 @@
 import React, { FunctionComponent } from 'react'
-import { Image } from 'react-native'
+import {
+  Image,
+  Platform,
+  View,
+  KeyboardAvoidingView,
+  ScrollView,
+} from 'react-native'
+import Icon from 'react-native-vector-icons/Feather'
 
 import logo from './../../assets/logo.png'
-import { Container, Title } from './styles'
+import {
+  Container,
+  Title,
+  ForgotPasswordButton,
+  ForgotPasswordLabel,
+  CreateAccountButton,
+  CreateAccountLabel,
+} from './styles'
 
 import Input from './../../components/Input'
 import Button from './../../components/Button'
 
 const SignIn: FunctionComponent = () => {
   return (
-    <Container>
-      <Image source={logo} />
-      <Title>Faça seu logon</Title>
+    <>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+        enabled
+      >
+        <ScrollView
+          contentContainerStyle={{ flex: 1 }}
+          keyboardShouldPersistTaps="handled"
+        >
+          <Container>
+            <Image source={logo} />
+            <View>
+              <Title>Faça seu logon</Title>
+            </View>
 
-      <Input name="email" icon="mail" placeholder="E-mail" />
-      <Input name="password" icon="lock" placeholder="Senha" />
+            <Input name="email" icon="mail" placeholder="E-mail" />
+            <Input name="password" icon="lock" placeholder="Senha" />
 
-      <Button>Entrar</Button>
-    </Container>
+            <Button>Entrar</Button>
+
+            <ForgotPasswordButton>
+              <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+            </ForgotPasswordButton>
+          </Container>
+        </ScrollView>
+      </KeyboardAvoidingView>
+
+      <CreateAccountButton>
+        <Icon name="log-in" size={20} color="#ff9000" />
+        <CreateAccountLabel>Criar uma conta</CreateAccountLabel>
+      </CreateAccountButton>
+    </>
   )
 }
 
